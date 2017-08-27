a3cb49894ce13559d56f6992578576392921aceb
Andrew Larcombe <andrew@andrewl.net>
2017-08-27 22:51:13 +0100

    initial commit of script

---
+++ brewpi_get_status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOGFILE=/home/brewpi/logs/stdout.txt
+
+DATE=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d " " -f 1-4)
+BEER_TEMP=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d ":" -f 4 | cut -d "," -f 1)
+FRIDGE_TEMP=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d ":" -f 5 | cut -d "," -f 1)
+
+echo "$DATE Beer Temp $BEER_TEMP Fridge Temp $FRIDGE_TEMP"
