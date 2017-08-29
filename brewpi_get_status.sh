95c5457a23c172b2a7ac16c18acbe4a89ac7045b
Andrew Larcombe <andrew@andrewl.net>
2017-08-29 09:38:33 +0100

    Fixes issue with wrong value being pulled from logfile for fridge temp.

--- brewpi_get_status.sh
+++ brewpi_get_status.sh
@@ -4,6 +4,6 @@ LOGFILE=/home/brewpi/logs/stdout.txt
 
 DATE=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d " " -f 1-4)
 BEER_TEMP=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d ":" -f 4 | cut -d "," -f 1)
-FRIDGE_TEMP=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d ":" -f 5 | cut -d "," -f 1)
+FRIDGE_TEMP=$(grep "BeerTemp" $LOGFILE | tail -n 1 | cut -d ":" -f 7 | cut -d "," -f 1)
 
 echo "$DATE Beer Temp $BEER_TEMP Fridge Temp $FRIDGE_TEMP"
